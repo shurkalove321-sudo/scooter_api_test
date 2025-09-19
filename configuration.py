a46f6270f403386263e5d2f4c42f1d134d504910
Shurkalove321-sudo <shurkalove321@gmail.com>
2025-09-19 18:47:55 +0300

    Рефакторинг проекта: структура по спринту, добавлен PyTest, configuration, data, sender_stand_request

---
+++ configuration.py
@@ -0,0 +1,6 @@
+# базовый URL сервиса
+URL_SERVICE = "https://94874f10-c474-4b11-8ed3-221c35fa68ce.serverhub.praktikum-services.ru"
+
+# пути для запросов
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
